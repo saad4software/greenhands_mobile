38d59835dbe6bf2b7cd0459801af505835251780
Saad <saad4software@gmail.com>
2022-04-20 04:34:01 +0300

    validation

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:greenhands_mobile/blocs/api/api_bloc.dart';
 import 'package:greenhands_mobile/blocs/generic/generic_bloc.dart';
 import 'package:greenhands_mobile/extensions/widget_extension.dart';
 import 'package:greenhands_mobile/screens/pick_location_screen.dart';
@@ -22,20 +23,21 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   final _formKey = GlobalKey<FormState>();
 
   LatLng? position;
-
-  final genericBloc = GenericBloc();
-
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final confirmController = TextEditingController();
-  final firstNameController = TextEditingController();
-  final lastNameController = TextEditingController();
-  final mobileController = TextEditingController();
-  final addressController = TextEditingController();
+  final txtEmail = TextEditingController();
+  final txtPassword = TextEditingController();
+  final txtConfirm = TextEditingController();
+  final txtFirstName = TextEditingController();
+  final txtLastName = TextEditingController();
+  final txtMobile = TextEditingController();
+  final txtAddress = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     const appTitle = 'register_organizer';
+
+    var apiBloc = BlocProvider.of<ApiBloc>(context);
+    final genericBloc = GenericBloc();
+
 
     return Scaffold(
       appBar: AppBar(
@@ -53,7 +55,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   const Text("organizer_register_form", textAlign: TextAlign.center,),
                   const SizedBox(height: 16,),
                   FormFieldWidget(
-                    controller: emailController,
+                    controller: txtEmail,
                     label: "email",
                     validator: (val)=>val.validateEmail(),
                     keyboard: TextInputType.emailAddress,
@@ -61,7 +63,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   const SizedBox(height: 16,),
 
                   FormFieldWidget(
-                    controller: passwordController,
+                    controller: txtPassword,
                     label: "password",
                     validator: (val)=>val.validateRequired(),
                     isPassword: true,
@@ -70,34 +72,34 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
                   FormFieldWidget(
                     label: "confirm_password",
-                    validator: (val)=>val == passwordController.text ? null : "no_match",
+                    validator: (val)=>val.isNotEmpty() && val == txtPassword.text ? null : "no_match",
                     isPassword: true,
                   ),
                   const SizedBox(height: 16,),
 
                   FormFieldWidget(
-                    controller: firstNameController,
+                    controller: txtFirstName,
                     label: "first_name",
                     validator: (val)=>val.validateName(),
                   ),
                   const SizedBox(height: 8,),
 
                   FormFieldWidget(
-                    controller: lastNameController,
+                    controller: txtLastName,
                     label: "last_name",
                     validator: (val)=>val.validateName(),
                   ),
                   const SizedBox(height: 16,),
 
                   FormFieldWidget(
-                    controller: mobileController,
+                    controller: txtMobile,
                     label: "mobile",
                     validator: (val)=>val.validateMobile(),
                   ),
                   const SizedBox(height: 16,),
 
                   FormFieldWidget(
-                    controller: addressController,
+                    controller: txtAddress,
                     label: "address",
                     validator: (val)=>val.validateName(),
                   ),
@@ -107,6 +109,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                     builder: (context, state) {
                       return SizedBox(
                         child: FormField<LatLng>(
+                          validator: (val)=>position!=null ? null : "invalid_position",
                           builder: (state) {
                             return Column(
                               children: [
@@ -115,6 +118,18 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                                     position: position,
                                   ),
                                 ),
+                                if (state.hasError)
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 8, top: 10),
+                                    child: Text(
+                                      state.errorText!,
+                                      style: TextStyle(
+                                          fontStyle: FontStyle.normal,
+                                          fontSize: 13,
+                                          color: Colors.red[700],
+                                          height: 0.5),
+                                    ),
+                                  )
                               ],
                             );
                           }
@@ -137,6 +152,17 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                       if (_formKey.currentState!.validate()) {
 
                         widget.toast(context, "nice work");
+                        apiBloc.add(ApiRequest.single((p0) => p0.register(
+                            email: txtEmail.text,
+                          password: txtPassword.text,
+                          firstName: txtFirstName.text,
+                          lastName: txtLastName.text,
+                          phone: txtMobile.text,
+                          address: txtAddress.text,
+                          lat: position?.latitude,
+                          lng: position?.longitude
+
+                        )));
 
                       }
                     },
